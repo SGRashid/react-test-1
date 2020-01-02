64066c5681b8781d640b908e87e92fcdf6caea5e
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-01-02 22:05:55 +0500

    Added filter for cityes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import './Main.css';
 
+const cityList = [
+  "Абакан",
+  "Артем",
+  "Азов",
+  "Москва",
+  "Санкт-Петербург",
+  "Екатеринбург",
+  "Владивосток",
+  "Березовск",
+  "Тагил",
+  "Каменск-Уральский",
+  "Кушва",
+  "Сабик",
+  "Берлога",
+  "Шаля",
+  "Курган",
+  "Нягань",
+  "Саратов"
+];
+
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  };
+  filter = city => {
+    if (this.state.value === '') { return true; }
+    const len = this.state.value.length;
+    if (city.slice(0, len).toLowerCase() === this.state.value.toLowerCase()) { return true; }
+    return false;
+  };
   render() {
     return (
       <>
-        Hello
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        {cityList
+          .filter(this.filter)
+          .map(element => <p>{element}</p>)}
       </>
     )
   }
