bcc7d6373b780d9aa0e98da52682c223c2d921d9
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-01-05 19:04:10 +0500

    added eslint

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './Main.css';
 import Cityes from './Cityes';
 
@@ -7,35 +7,39 @@ const cityList = Cityes.data;
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', cityes: []};
+    this.state = { value: '', cityes: [] };
   }
+
   handleChange = (event) => {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   };
-  filter = city => {
+
+  filter = (city) => {
     if (this.state.value.length < 3) { return true; }
     const len = this.state.value.length;
     if (city.slice(0, len).toLowerCase() === this.state.value.toLowerCase()) { return true; }
     return false;
   };
-  cityAdd = city => {
+
+  cityAdd = (city) => {
     if (this.state.cityes.includes(city)) { return; }
     const newCityList = [...this.state.cityes, city].sort();
-    this.setState({value: '', cityes: newCityList});
-    // console.log([...this.state.cityes, city].sort());
+    this.setState({ value: '', cityes: newCityList });
     localStorage.setItem('cityes', JSON.stringify(newCityList));
   };
-  cityDelete = city => {
+
+  cityDelete = (city) => {
     const newCityList = [...this.state.cityes.filter(element => element !== city)];
-    this.setState({cityes: newCityList});
+    this.setState({ cityes: newCityList });
     localStorage.setItem('cityes', JSON.stringify(newCityList));
   };
+
   componentDidMount() {
-    // console.log(JSON.parse(localStorage.cityes));
     if (this.state.cityes.length > 0) { return; }
     const cityesFromStorage = JSON.parse(localStorage.cityes);
-    this.setState({'cityes': cityesFromStorage});
+    this.setState({ 'cityes': cityesFromStorage });
   }
+
   render() {
     return (
       <>
@@ -45,8 +49,8 @@ class Main extends React.Component {
             <button className='button'>Поиск</button>
             {
               cityList
-              .filter(this.filter)
-              .map(element => (
+                .filter(this.filter)
+                .map(element => (
                 <p
                   className='list-element'
                   onClick={() => this.cityAdd(element)}
@@ -61,7 +65,6 @@ class Main extends React.Component {
             <p><b>Выбрано:</b></p>
             {this.state.cityes.map(element => (
               <p className='choice-list-element'
-                // onClick={() => this.cityDelete(element)}
               >
                 {element}
                 <span className='x'
@@ -72,7 +75,7 @@ class Main extends React.Component {
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
