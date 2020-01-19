327c79858955354d7972ab16358fd99f7ca2359b
Sergey Gabdullin <s.gabdullin@gmail.com>
2020-01-19 15:37:10 +0500

    Updates

---
+++ precache-manifest.8b7c2876c37bc492bd8908770703b6d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ffbbd2cb3f1e02ca50c7729b961091",
+    "url": "/react-test-1/index.html"
+  },
+  {
+    "revision": "6be1bffe323b23fa5b15",
+    "url": "/react-test-1/static/css/main.9586812b.chunk.css"
+  },
+  {
+    "revision": "2aceeaab40e873e08e22",
+    "url": "/react-test-1/static/js/2.8c47bd1d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-test-1/static/js/2.8c47bd1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6be1bffe323b23fa5b15",
+    "url": "/react-test-1/static/js/main.89634f17.chunk.js"
+  },
+  {
+    "revision": "5fcb648459ec60592431",
+    "url": "/react-test-1/static/js/runtime-main.a603e96b.js"
+  }
+]);
